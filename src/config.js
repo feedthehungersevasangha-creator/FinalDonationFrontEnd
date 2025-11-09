ddb088fe1622c80ac70f7a37ecbaac34cf726740
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-11-09 18:01:26 +0530

    Update config.js
    razor pay url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
   API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
-  Razor_Pay:import.meta.env.REACT_APP_RAZORPAY_KEY_ID
+  Razor_Pay:import.meta.env.VITE_RAZORPAY_KEY_ID
 };
 
 export default config;
