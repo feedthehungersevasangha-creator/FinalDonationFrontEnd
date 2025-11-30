3615eca1c43df56445c06bec9bdfd4d1e92c1c5f
feedthehungersevasangha-creator <feedthehungersevasangha@gmail.com>
2025-11-30 23:35:50 +0530

    Update DonationModal.jsx

--- src/components/DonationModal.jsx
+++ src/components/DonationModal.jsx
@@ -264,7 +264,7 @@ function DonationModal() {
       {...register("startDay")}
       className="border rounded px-3 py-2 w-full"
     >
-      <option value="">Please Select Next Payment Date </option>
+      <option value="">Please Select E-Mandate  Date </option>
       <option value="5">5th of Next Month</option>
       <option value="10">10th of Next Month</option>
       <option value="15">15th of Next Month</option>
@@ -418,6 +418,7 @@ function DonationModal() {
 }
 
 export default DonationModal;
+
 
 
 
